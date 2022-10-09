28f244cb28f1dd0c5ea35079a58aede9d3698c86
Pulkit Shrivastava <pulkitshrivastava9@gmail.com>
2022-10-09 18:27:37 +0530

    Login Auth API

--- server/routes/AuthApi.js
+++ server/routes/AuthApi.js
@@ -1,6 +1,8 @@
 import bcrypt from "bcrypt";
 import { Router } from "express";
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
 const router = Router();
 
 router.post("/register", async (req, res) => {
@@ -23,6 +25,29 @@ router.post("/register", async (req, res) => {
   });
   await user.save();
   res.status(201).json({ message: "user is created" });
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    res.status(406).json({ message: "credentials not found" });
+    return;
+  }
+
+  const matched = await bcrypt.compare(password, user.password);
+  if (!matched) {
+    res.status(406).json({ message: "credentials not found" });
+    return;
+  }
+
+  const payload = {
+    username: email,
+    _id: user._id,
+  };
+  const token = jwt.sign(payload, "some secret.");
+  res.json({ message: "succesfully logged in.", token });
 });
 
 export default router;
