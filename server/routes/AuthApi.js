29cb27fdf3f7ce5ef2aa5c0bf7dcf12bea85acae
Pulkit Shrivastava <pulkitshrivastava9@gmail.com>
2022-10-09 17:56:54 +0530

    Register User Feature: Schema and API

--- server/routes/AuthApi.js
+++ server/routes/AuthApi.js
@@ -1,8 +1,28 @@
+import bcrypt from "bcrypt";
 import { Router } from "express";
+import User from "../models/User.js";
 const router = Router();
 
-router.post("/register", (req, res) => {
-  res.json({ message: "user is created" });
+router.post("/register", async (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(406).json({ message: "User already exists." });
+    return;
+  }
+  const saltRounds = 10;
+  const salt = await bcrypt.genSaltSync(saltRounds);
+  const hashedPassword = await bcrypt.hashSync(password, salt);
+
+  const user = await User({
+    email,
+    password: hashedPassword,
+    firstName,
+    lastName,
+  });
+  await user.save();
+  res.status(201).json({ message: "user is created" });
 });
 
 export default router;
